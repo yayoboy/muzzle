880034c39c279754dad00e4b99b7b2fdd7279af1
yayoboy <esoglobine@gmail.com>
2026-03-01 17:50:21 +0100

    chore: untrack build artifacts and fix .gitignore

--- apps/server/dist/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const app_1 = require("./app");
-const PORT = process.env.PORT || 3001;
-(0, app_1.createApp)().listen(PORT, () => console.log(`Muzzle server running on ${PORT}`));
